a22c3edac72934ed03fdded9c47bebb71e2e17e6
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-10-01 13:29:58 +0300

    init(DatingSite): created repo

---
+++ src/main/java/org/spring/datingsite/DatingSiteApplication.java
@@ -0,0 +1,13 @@
+package org.spring.datingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatingSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/spring/datingsite/DatingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.datingsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
